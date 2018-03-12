369a1a8f29a3d5697bc97950801873551227fe83
Milton Andrade <miligraf@gmail.com>
2018-03-12 15:31:09 -0400

    Use handlebars partials; Setup express middleware to log requests

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log();
+  fs.appendFileSync('server.log', log + '\n')
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Welcome Page',
-    currentYear: new Date().getFullYear(),
     welcomeMessage: 'Welcome J!'
   });
 });
@@ -17,7 +40,6 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
   });
 });
 
